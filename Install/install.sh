dcb52fc675d668d0ac7c454ca08c4fdbcdb2d076
HarshVaragiya <harshvaragiya8@gmail.com>
2020-07-06 00:53:20 +0530

    Added Makefile and updated install script to install nmap

--- Install/install.sh
+++ Install/install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 echo "[+] Install Script for Ariadne"
 
-echo "[+] Installing golang,git"
-sudo apt install golang git
+echo '------------------------------------------------'
+echo "[+] Installing golang,git,nmap"
+sudo apt install golang git nmap
 
+echo '------------------------------------------------'
 echo "[+] Getting necessary dependencies"
 read -rsp $'[+] Press any key to continue (or ctrl+c to quit)...\n' -n1 key
 go get github.com/sirupsen/logrus
@@ -12,11 +14,19 @@ go get github.com/akamensky/argparse
 go get github.com/Ullaakut/nmap
 go get github.com/elastic/go-elasticsearch
 
+echo '------------------------------------------------'
 echo "[+] Patching Gobuster"
 read -rsp $'[+] Press any key to continue (or ctrl+c to quit)...\n' -n1 key
-patch -u -b ~/go/src/github.com/OJ/gobuster/libgobuster/libgobuster.go -i $PWD/libgobuster.patch
+patch -u -b ~/go/src/github.com/OJ/gobuster/libgobuster/libgobuster.go -i "$PWD/libgobuster.patch"
 echo "[+] Patched libgobuster/libgobuster.go"
 
-echo "[+] To build Ariadne run"
-echo "$ go install main.go"
+echo '------------------------------------------------'
+echo "[+] To build Ariadne:"
+echo "$ make build"
 echo "[+] from the Ariadne directory"
+
+echo '------------------------------------------------'
+echo "[+] To run Ariadne:"
+echo "$ make run"
+echo "[+] from the Ariadne directory"
+
---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	go build -o bin/Ariadne main.go
+
+run:
+	go run main.go
+
+install:
+	go install .
